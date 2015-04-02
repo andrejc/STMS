51fadf30bd302be5de9c59547245d97a6f9e742f
Andrej Cvoro <acvoro@gmail.com>
2015-04-02 14:18:43 +0200

    Add Task entity

---
+++ src/STMS/STMSBundle/Entity/Task.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace STMS\STMSBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Task
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="date")
+     */
+    private $date;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="hours", type="float")
+     */
+    private $hours;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="notes", type="string", length=2000)
+     */
+    private $notes;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Task
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Task
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set hours
+     *
+     * @param float $hours
+     * @return Task
+     */
+    public function setHours($hours)
+    {
+        $this->hours = $hours;
+
+        return $this;
+    }
+
+    /**
+     * Get hours
+     *
+     * @return float 
+     */
+    public function getHours()
+    {
+        return $this->hours;
+    }
+
+    /**
+     * Set notes
+     *
+     * @param string $notes
+     * @return Task
+     */
+    public function setNotes($notes)
+    {
+        $this->notes = $notes;
+
+        return $this;
+    }
+
+    /**
+     * Get notes
+     *
+     * @return string 
+     */
+    public function getNotes()
+    {
+        return $this->notes;
+    }
+}
